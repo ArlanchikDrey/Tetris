f1632727a809c1a1714be731af65487b8f1f1215
ArlanchikDrey <arlanchikdrey@yandex.ru>
2020-10-30 01:02:03 +0300

    fix music lifecycle in mainactivity and added game page

--- app/src/main/java/com/xxx_market/tetris/activities/GameActivity.kt
+++ app/src/main/java/com/xxx_market/tetris/activities/GameActivity.kt
@@ -1,12 +1,17 @@
 package com.xxx_market.tetris.activities
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.xxx_market.tetris.R
-import com.xxx_market.tetris.components.DaggerFrameComponent
+import com.xxx_market.tetris.components.DaggerGameActivityComponent
+import com.xxx_market.tetris.customViews.TetrisView
 import com.xxx_market.tetris.databinding.GameMainBinding
+import com.xxx_market.tetris.model.AppPreferences
 import com.xxx_market.tetris.model.Frame
 import com.xxx_market.tetris.model.ModelGameScore
+import com.xxx_market.tetris.modules.AppModule
 import com.xxx_market.tetris.modules.FrameModule
 import com.xxx_market.tetris.presenter.GamePagePresenter
 import com.xxx_market.tetris.view.GamePageView
@@ -19,7 +24,11 @@ import javax.inject.Inject
 class GameActivity : MvpAppCompatActivity(), GamePageView {
 
     private lateinit var modelScore: ModelGameScore
+    private lateinit var tetrisView: TetrisView
     private lateinit var binding: GameMainBinding
+
+    @Inject
+    lateinit var appPreferences: AppPreferences
 
     @Inject
     lateinit var frame: Frame
@@ -34,10 +43,53 @@ class GameActivity : MvpAppCompatActivity(), GamePageView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,
+        binding = DataBindingUtil.setContentView(
+            this,
             R.layout.game_main
         )
-        DaggerFrameComponent.builder().frameModule(FrameModule(10)).build().inject(this)
+        DaggerGameActivityComponent.builder().appModule(AppModule(application))
+            .frameModule(FrameModule(10)).build().inject(this)
+
+        presenter.setPreferences(appPreferences)
+        presenter.initMusic(applicationContext)
+        tetrisView = binding.viewTetris
+        tetrisView.setActivity(this)
+        tetrisView.setModel(presenter)
+        tetrisView.setOnTouchListener(this::onTetrisViewTouch)
+
+    }
+
+    private fun moveTetromino(motion: GamePagePresenter.Motions) {
+        if (presenter.isGameActive()) {
+            tetrisView.setGameCommand(motion)
+        }
+    }
+
+    private fun resolveTouchDirection(view: View, event: MotionEvent): Int {
+        val x = event.x / view.width
+        val y = event.y / view.height
+        val direction: Int
+        direction = if (y > x) {
+            if (x > 1 - y) 2 else 0
+        } else {
+            if (x > 1 - y) 3 else 1
+        }
+        return direction
+    }
+
+    private fun onTetrisViewTouch(view: View, event: MotionEvent): Boolean {
+        if (presenter.isGameOver() || presenter.isGameAwaitingStart()) {
+            presenter.startGame()
+            tetrisView.setGameCommandWithDelay(GamePagePresenter.Motions.DOWN)
+        } else if (presenter.isGameActive()) {
+            when (resolveTouchDirection(view, event)) {
+                0 -> moveTetromino(GamePagePresenter.Motions.LEFT)
+                1 -> moveTetromino(GamePagePresenter.Motions.ROTATE)
+                2 -> moveTetromino(GamePagePresenter.Motions.DOWN)
+                3 -> moveTetromino(GamePagePresenter.Motions.RIGHT)
+            }
+        }
+        return true
     }
 
 }
--- app/src/main/java/com/xxx_market/tetris/activities/MainActivity.kt
+++ app/src/main/java/com/xxx_market/tetris/activities/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.xxx_market.tetris.activities
 
+import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.text.SpannableString
-import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.xxx_market.tetris.R
-import com.xxx_market.tetris.components.DaggerFrameComponent
 import com.xxx_market.tetris.components.DaggerMainActivityComponent
 import com.xxx_market.tetris.databinding.ActivityMainBinding
 import com.xxx_market.tetris.model.AppPreferences
-import com.xxx_market.tetris.model.Frame
 import com.xxx_market.tetris.model.ModelScore
 import com.xxx_market.tetris.modules.AppModule
 import com.xxx_market.tetris.presenter.MainPagePresenter
 import com.xxx_market.tetris.view.MainPageView
-import kotlinx.android.synthetic.main.activity_main.*
 import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
 import moxy.presenter.ProvidePresenter
@@ -25,6 +23,8 @@ class MainActivity : MvpAppCompatActivity(), MainPageView {
 
     private lateinit var modelScore: ModelScore
     private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
+
     @Inject
     lateinit var appPreferences: AppPreferences
 
@@ -38,13 +38,23 @@ class MainActivity : MvpAppCompatActivity(), MainPageView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,
+        binding = DataBindingUtil.setContentView(
+            this,
             R.layout.activity_main
         )
         DaggerMainActivityComponent.builder().appModule(AppModule(application)).build().inject(this)
-        presenter.drawTitle(applicationContext)
-        presenter.initMusic(applicationContext)
+        presenter.drawTitle(resources.getString(R.string.app_name))
+        presenter.initMusic()
         presenter.getScore(appPreferences)
+
+        binding.btnNewGame.setOnClickListener {
+            startActivity(Intent(this, GameActivity::class.java))
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.initMusic()
     }
 
     override fun onPause() {
@@ -54,16 +64,47 @@ class MainActivity : MvpAppCompatActivity(), MainPageView {
 
     override fun onStop() {
         super.onStop()
-        presenter.stopMusic()
+        mediaPlayer?.stop()
     }
 
     override fun drawTitle(spans: SpannableString) {
-        mainActivity_title_0.text = spans
+        binding.mainActivityTitle0.text = spans
     }
 
     override fun getScore(score: Int) {
         modelScore = ModelScore(score = "${resources.getString(R.string.bill)} $score")
         binding.score = modelScore
+    }
+
+    override fun startMusic() {
+        if (presenter.getPosition() != null) {
+            if (mediaPlayer == null) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.galaxy)
+                mediaPlayer?.seekTo(presenter.getPosition()!!)
+                mediaPlayer?.start()
+                mediaPlayer?.isLooping = true
+            }
+        } else {
+            if (mediaPlayer == null) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.galaxy)
+                mediaPlayer?.start()
+                mediaPlayer?.isLooping = true
+            }
+        }
+    }
+
+    override fun pauseMusic() {
+        if (mediaPlayer != null) {
+            if (mediaPlayer!!.isPlaying) {
+                mediaPlayer?.stop()
+                presenter.setPosition(position = mediaPlayer?.currentPosition)
+                mediaPlayer = null
+            }
+        }
+    }
+
+    override fun resetMusic() {
+        mediaPlayer?.reset()
     }
 
 }
